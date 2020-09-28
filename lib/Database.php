21794d932e6c0afea3ada7e39f23a0c0ba8cafec
Martijn Goedhart <goedhart.martijn@gmail.com>
2020-09-28 20:21:09 +0200

    change for test

--- lib/Database.php
+++ lib/Database.php
@@ -4,7 +4,7 @@ namespace LaRouxOf;
 
 use PDO;
 
-require_once __DIR__ . "/../settings.php";
+require_once __DIR__ . "/../src/settings.php";
 
 final class Database
 {
@@ -14,7 +14,8 @@ final class Database
 	{
 		if(self::$connection != null)
 			return self::$connection;
-		$dsn = 'mysql:dbname=' . Settings::SQL_DB . ';host=' . Settings::SQL_HOST;
+		#$dsn = 'mysql:dbname=' . Settings::SQL_DB . ';host=' . Settings::SQL_HOST;
+		$dsn = 'sqlite:' . __DIR__ . '/../test.db';
 		self::$connection = new PDO($dsn, Settings::SQL_USERNAME, Settings::SQL_PASSWORD);
 		return self::$connection;
 	}
